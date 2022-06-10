26cd10e60e9fa57c927c5d036238f441e365067c
fateme-zka <fatemezahrakalantarii@gmail.com>
2022-06-11 02:39:38 +0430

    image-adding funtionality set, and created_at field added to Photo(Model)

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -10,8 +11,9 @@ class Category(models.Model):
 
 class Photo(models.Model):
     title = models.CharField(max_length=300)
-    image = models.ImageField(null=False, blank=False, )
+    image = models.ImageField(null=False, blank=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Photo, Category
 
 
 def gallery(request):
-    photos = Photo.objects.all()
-    categories = Category.objects.all()
+    photos = Photo.objects.all().order_by('-created_at')
+    categories = Category.objects.all().order_by('-id')
 
     context = {
         'photos': photos,
@@ -21,4 +21,27 @@ def view_photo(request, pk):
 
 
 def add_photo(request):
-    return render(request, 'photos/add_photo.html', {})
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        # print('data>>>', data)
+        # print('images>>>', images)
+
+        if data['category_new'] != '':  # if user created a new category
+            category, create = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = Category.objects.get(pk=data['category'])  # because {{data['category']}} is return category's id
+
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                title=data['title'],
+                image=image,
+            )
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add_photo.html', context)
